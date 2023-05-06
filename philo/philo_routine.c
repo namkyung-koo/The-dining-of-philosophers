12d66e8d0012d32220fcc938a3323c4ba84c72ac
koonamkyung <united94@naver.com>
2023-05-06 13:41:43 +0900

    Add : check_finish

--- philo/philo_routine.c
+++ philo/philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 16:58:34 by nakoo             #+#    #+#             */
-/*   Updated: 2023/05/05 21:06:58 by nakoo            ###   ########.fr       */
+/*   Updated: 2023/05/06 13:41:17 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 static int	is_full(t_philo *philo)
 {
+	if (check_finish(philo))
+		return (1);
 	if (philo->share->args->must_eat_count == philo->eat_count)
 	{
 		pthread_mutex_lock(&(philo->share->lock_m));
@@ -24,12 +26,26 @@ static int	is_full(t_philo *philo)
 	return (0);
 }
 
+static int	check_finish(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->share->finish_m));
+	if (!(philo->share->running))
+	{
+		pthread_mutex_unlock(&(philo->share->finish_m));
+		return (1);
+	}
+	pthread_mutex_unlock(&(philo->share->finish_m));
+	return (0);
+}
+
 static int	is_end(void *ptr)
 {
 	t_philo		*philo;
 	uint64_t	now;
 
 	philo = (t_philo *)ptr;
+	if (check_finish(philo))
+		return (1);
 	now = get_time();
 	if (now - philo->last_meal >= (uint64_t)philo->share->args->time_to_die)
 	{
